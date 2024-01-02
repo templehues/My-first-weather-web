22773ce03e8b681f082e66d25af8690e70718047
Huey <phansineo2001@gmail.com>
2024-01-02 20:02:56 +0200

    added the API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+function refreshWeatherData(response) {
+  console.log(response.data);
+}
+
+function searchCity(city) {
+  let apiKey = "1of0t314d94be2d47af94e3feebe0e6d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeatherData);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
