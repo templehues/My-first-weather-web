22facd081bf1b84abfa5679bbad8c435574fcd28
Huey <phansineo2001@gmail.com>
2024-01-02 20:14:36 +0200

    Added a response function and more ID elements

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 function refreshWeatherData(response) {
-  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
@@ -11,10 +16,11 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Cape Town");
