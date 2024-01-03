58aa4e1a771ddf9f7a38d6c80ca4381337e98737
Huey <phansineo2001@gmail.com>
2024-01-03 21:31:25 +0200

    use JS to inject the forecast to the HTML

--- src/index.js
+++ src/index.js
@@ -56,3 +56,18 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Cape Town");
+
+let forecast = document.querySelector("#forecast");
+
+forecast.innerHTML = `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">Thu</div>
+    <div class="weather-forecast-icon">☀️</div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong>31°</strong>
+      </div>
+      <div class="weather-forecast-temperature">18°</div>
+    </div>
+  </div>
+`;
